fc39c79dd07a2bbcccf7ba2c5fbe5602645728cc
Nathalia Schwertner <nathalia_13@outlook.com>
2023-08-31 23:54:46 -0300

    adiciona tela minhas tags

--- lib/minhas_tags.dart
+++ lib/minhas_tags.dart
@@ -7,8 +7,45 @@ class MinhasTagsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Minhas Tags'),
       ),
-      body: Center(
-        child: Text('Aqui estão suas tags!'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'MINHAS TAGS',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Image.asset('images/minhastags.PNG', width: 400, height: 200),
+            SizedBox(height: 20),
+            Text(
+              'ATENÇÃO: Tag parafusadeira não se encontra no veículo',
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    // Ação do botão 1
+                  },
+                  child: Text('VOLTAR'),
+                ),
+                SizedBox(
+                    width: 10), // Ajuste o valor de width conforme necessário
+                ElevatedButton(
+                  onPressed: () {
+                    // Ação do botão 2
+                  },
+                  child: Text('LOCALIZAR'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
